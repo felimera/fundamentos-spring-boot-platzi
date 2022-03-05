80d84cf37779fa1d1f19dec0f872f4bd4088c5c4
andres.mera <andres.mera@sitis.com.co>
2022-03-04 22:22:38 -0500

    Pagination con Sprint Boot.

--- src/main/java/com/fundamentosplatzi/sprintboot/fundamentos/controller/UserRestController.java
+++ src/main/java/com/fundamentosplatzi/sprintboot/fundamentos/controller/UserRestController.java
@@ -5,6 +5,8 @@ import com.fundamentosplatzi.sprintboot.fundamentos.caseuse.DeleteUser;
 import com.fundamentosplatzi.sprintboot.fundamentos.caseuse.GetUser;
 import com.fundamentosplatzi.sprintboot.fundamentos.caseuse.UpdateUser;
 import com.fundamentosplatzi.sprintboot.fundamentos.entity.User;
+import com.fundamentosplatzi.sprintboot.fundamentos.repository.UserRepository;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,12 +21,14 @@ public class UserRestController {
     private CreateUser createUser;
     private DeleteUser deleteUser;
     private UpdateUser updateUser;
+    private UserRepository userRepository;
 
-    public UserRestController(GetUser getUser, CreateUser createUser, DeleteUser deleteUser, UpdateUser updateUser) {
+    public UserRestController(GetUser getUser, CreateUser createUser, DeleteUser deleteUser, UpdateUser updateUser, UserRepository userRepository) {
         this.getUser = getUser;
         this.createUser = createUser;
         this.deleteUser = deleteUser;
         this.updateUser = updateUser;
+        this.userRepository = userRepository;
     }
 
     @GetMapping(path = "/")
@@ -48,4 +52,8 @@ public class UserRestController {
         return ResponseEntity.ok().body(updateUser.update(newUser, id));
     }
 
+    @GetMapping("/pageable")
+    List<User> getUserPageable(@RequestParam int page, @RequestParam int size) {
+        return userRepository.findAll(PageRequest.of(page, size)).getContent();
+    }
 }
--- src/main/java/com/fundamentosplatzi/sprintboot/fundamentos/repository/UserRepository.java
+++ src/main/java/com/fundamentosplatzi/sprintboot/fundamentos/repository/UserRepository.java
@@ -3,8 +3,8 @@ package com.fundamentosplatzi.sprintboot.fundamentos.repository;
 import com.fundamentosplatzi.sprintboot.fundamentos.dto.UserDto;
 import com.fundamentosplatzi.sprintboot.fundamentos.entity.User;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
     @Query(" SELECT u FROM User u WHERE u.email = ?1 ")
     Optional<User> findByUserEmail(String email);
 
@@ -40,4 +40,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
             " AND" +
             " u.email=:parametroEmail")
     Optional<UserDto> getAllByBirthDateAndEmail(@Param("parametroFecha") LocalDate date, @Param("parametroEmail") String email);
+
+    List<User> findAll();
 }
